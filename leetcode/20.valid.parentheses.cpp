8bb296e4fcf7bab84f392177b4146c8815af418f
Weiwen <gwengww@gmail.com>
2018-05-01 23:35:40 -0500

    add 20

---
+++ leetcode/20.valid.parentheses.cpp
@@ -0,0 +1,24 @@
+//
+// Created by gwengww on 5/1/18.
+// beat 95.53%
+// space O(N)
+// time O(N)
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> aStack;
+        if (s.size() == 0) return true;
+        if (s.size()%2==1) return false;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == '{' || s[i] == '(' || s[i] == '[') aStack.push(s[i]);
+            else {
+                if (aStack.empty()) return false;
+                if (s[i] == '}' && aStack.top() == '{' ||
+                    s[i] == ']' && aStack.top() == '[' ||
+                    s[i] == ')' && aStack.top() == '(') aStack.pop();
+            }
+        }
+        return aStack.empty();
+    }
+};
