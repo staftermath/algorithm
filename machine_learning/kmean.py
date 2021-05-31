53d396f306407f1e8dc23e2eb66ef4b6bf0f1e53
Weiwen Gu <gwengww@gmail.com>
2021-05-31 12:04:29 -0500

    feature: implement k mean algorith

---
+++ machine_learning/kmean.py
@@ -0,0 +1,102 @@
+import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.preprocessing import normalize
+
+
+class KMean:
+
+    def __init__(self, k: int, e: float = 0.0001):
+        self.k = k
+        self.e = e
+
+    def fit(self, data: np.ndarray, init="default") -> (np.ndarray, np.ndarray):
+
+        centers = self._initialize(data, init=init)
+        return self._assign(data, centers)
+
+    def _initialize(self, data: np.ndarray, init: str = "default"):
+        if init == "default":
+            return _initialize_default(data, self.k)
+        elif init == "random_partition":
+            return _initialize_random_partition(data, self.k)
+        elif init == "kmean++":
+            return _initialize_kmean_plus_plus(data, self.k)
+        else:
+            raise ValueError(f"Invald initialization type {init}")
+
+    def _assign(self, data: np.ndarray, centers: np.ndarray):
+        delta = np.ones(self.k)
+        n, d = data.shape
+        allocation = None
+        while np.max(delta) > self.e:
+            allocation = np.argmin(euclidean_distances(data, centers), axis=1)
+            new_centers = np.ones((self.k, d))
+            for i in range(self.k):
+                if data[allocation == i].shape[0] == 0:
+                    new_centers[i] = data[np.random.choice(range(n))]
+                else:
+                    new_centers[i] = np.mean(data[allocation == i], axis=0)
+
+            delta = np.max(np.abs(new_centers - centers))
+            centers = new_centers
+
+        return centers, allocation
+
+    def _kmean_pp_assign(self, data: np.ndarray, centers: np.ndarray):
+        delta = np.ones(self.k)
+        n, d = data.shape
+        allocation = None
+
+        def _map(x):
+            return np.random.choice(1, prob=x)
+
+        np.vectorize(_map)
+
+        while np.max(delta) > self.e:
+            prob = normalize(np.reciprocal(euclidean_distances(data, centers) + 0.0000001), norm="l1", axis=1, copy=False)
+            allocation = np.ones(n)
+            for i in range(n):
+                allocation[i] = np.random.choice(range(self.k), p=prob[i])
+
+            new_centers = np.ones((self.k, d))
+            for i in range(self.k):
+                new_centers[i] = np.mean(data[allocation == i], axis=0)
+
+            delta = np.max(np.abs(new_centers - centers))
+            centers = new_centers
+
+        return centers, allocation
+
+
+def _initialize_default(data: np.ndarray, k: int, seed: int=123):
+    np.random.seed(seed)
+    centers = data[np.random.choice(range(data.shape[0]), k, replace=False)]
+    return centers
+
+
+def _initialize_random_partition(data: np.ndarray, k: int, seed: int=123):
+    np.random.seed(seed)
+    d = data.shape[1]
+    label = np.random.choice(range(k), data.shape[0])
+    centers = np.ones((k, d))
+    for i in range(k):
+        centers[i] = np.mean(data[label == i], axis=0)
+
+    return centers
+
+
+def _initialize_kmean_plus_plus(data: np.ndarray, k: int, seed: int = 123):
+    centers = _initialize_default(data, k, seed)
+    prob = normalize(np.reciprocal(euclidean_distances(data, centers) + 0.0000001), norm="l1", axis=1, copy=False)
+    n, d = data.shape
+    allocation = np.ones(n)
+    for i in range(n):
+        allocation[i] = np.random.choice(range(k), p=prob[i])
+
+    for i in range(k):
+        if data[allocation == i].shape[0] == 0:
+            centers[i] = data[np.random.choice(range(n))]
+        else:
+            centers[i] = np.mean(data[allocation == i], axis=0)
+
+    return centers
---
+++ machine_learning/test_kmean.py
@@ -0,0 +1,84 @@
+import numpy as np
+from numpy.testing import assert_almost_equal, assert_array_equal
+from sklearn import datasets
+import pytest
+
+from machine_learning.kmean import KMean
+
+def test_kmean():
+    kmean = KMean(3)
+    np.random.seed(123)
+    data = np.random.rand(10, 10)
+    centers, clusters = kmean.fit(data)
+    expected_centers = np.array(
+        [[0.35802895, 0.27465978, 0.37407412, 0.45425565, 0.64606972,
+          0.28135829, 0.45469279, 0.93947447, 0.73182257, 0.5573656 ],
+         [0.68289148, 0.43603794, 0.42587748, 0.61300191, 0.7809057 ,
+          0.25315072, 0.87222352, 0.46424806, 0.33757743, 0.48228724],
+         [0.36960757, 0.82957965, 0.5506058 , 0.51511403, 0.49961501,
+          0.58214646, 0.3377688 , 0.28107386, 0.39990279, 0.51839955]]
+    )
+    assert_almost_equal(expected_centers, centers)
+    expected_clusters = np.array([1, 2, 2, 0, 0, 2, 1, 2, 2, 2])
+    assert_array_equal(expected_clusters, clusters)
+
+
+@pytest.fixture()
+def iris():
+    data = datasets.load_iris()
+    return data
+
+
+def test_kmean_cluster_iris_correctly(iris):
+    kmean = KMean(3)
+    def _map(x):
+        if x == 1:
+            return 2
+        elif x == 2:
+            return 1
+        else:
+            return 0
+
+    np.random.seed(123)
+    mapping = np.vectorize(_map)
+    centers, clusters = kmean.fit(iris["data"])
+    clusters = mapping(clusters)
+    result = np.mean(iris["target"] == clusters)
+    assert result == 0.8933333333333333
+
+
+def test_kmean_cluster_iris_using_random_partition_init(iris):
+    kmean = KMean(3)
+
+    def _map(x):
+        if x == 2:
+            return 0
+        elif x == 1:
+            return 1
+        else:
+            return 2
+
+    mapping = np.vectorize(_map)
+    centers, clusters = kmean.fit(iris["data"], init="random_partition")
+    clusters = mapping(clusters)
+    result = np.mean(iris["target"] == clusters)
+    assert result == 0.8933333333333333
+
+
+def test_kmean_cluster_iris_using_kmean_plus_plus(iris):
+    kmean = KMean(3)
+
+    def _map(x):
+        if x == 1:
+            return 2
+        elif x == 2:
+            return 1
+        else:
+            return 0
+
+    np.random.seed(321)
+    mapping = np.vectorize(_map)
+    centers, clusters = kmean.fit(iris["data"], init="kmean++")
+    clusters = mapping(clusters)
+    result = np.mean(iris["target"] == clusters)
+    assert result == 0.8866666666666667
